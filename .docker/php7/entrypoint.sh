cb0b3a852d3433a0e4b4d60f677ea8485b3de106
Vitor Mattos <vitor@php.rio>
2019-07-07 18:31:40 -0300

    Inclusão do composer.phar na imagem

--- .docker/php7/entrypoint.sh
+++ .docker/php7/entrypoint.sh
@@ -1,10 +1,7 @@
 if [ ! -d "vendor" ]; then
-    if [ ! -f "composer.phar" ]; then
-        curl https://getcomposer.org/composer.phar --output /var/www/csp/composer.phar
-        php composer.phar global require hirak/prestissimo
-    fi
+    composer global require hirak/prestissimo
     export COMPOSER_ALLOW_SUPERUSER=1
-    php composer.phar install
+    composer install
 fi
 php banner.php
 php-fpm
