d750a4922fb83ca8ce0e5827f4a558a7d6f65f8f
Vitor Mattos <vitor@php.rio>
2019-07-07 17:59:30 -0300

    Otimização de montagem de ambiente Apenas executa composer se não existir vendor e só baixa o composer se já não existir

--- .docker/php7/entrypoint.sh
+++ .docker/php7/entrypoint.sh
@@ -1,6 +1,10 @@
-curl https://getcomposer.org/composer.phar --output /app/composer.phar
-php composer.phar global require hirak/prestissimo
-export COMPOSER_ALLOW_SUPERUSER=1
-php composer.phar install
+if [ ! -d "vendor" ]; then
+    if [ ! -f "composer.phar" ]; then
+        curl https://getcomposer.org/composer.phar --output /var/www/csp/composer.phar
+        php composer.phar global require hirak/prestissimo
+    fi
+    export COMPOSER_ALLOW_SUPERUSER=1
+    php composer.phar install
+fi
 php banner.php
 php-fpm
